9ee8109cc4d139075eaefa633a6cc32c75678581
Tejas Patel <pateltejas.2005@gmail.com>
2026-03-21 16:59:15 +0530

    v2.0

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'places.googleapis.com',
+            }
+        ],
+    },
+    experimental: {
+        serverComponentsExternalPackages: ['mongoose']
+    }
+};
 
 export default nextConfig;
